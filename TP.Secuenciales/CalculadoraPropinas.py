fa322f389743d35194207691dc53e1329f6aeda2
Leonel Ponce <vm8406100@gmail.com>
2025-08-21 20:51:14 -0300

    Reorganización de carpetas y archivos

--- TP.Secuenciales/CalculadoraPropinas.py
+++
@@ -1,7 +0,0 @@
-monto_total = float(input("Ingrese el monto total de la cuenta: "))
-propina_10 = monto_total * 0.10
-propina_15 = monto_total * 0.15
-print(f"Propina sugerida (10%): {propina_10}")
-print(f"Total a pagar (10%): {propina_10 + monto_total}")
-print(f"Propina sugerida (15%): {propina_15}")
-print(f"Total a pagar (15%): {propina_15 + monto_total}")
